f973bc33dc14647ada8cbb5185e8b49bdaf7305f
jiahuaitang <jhuaitang@163.com>
2022-06-20 10:48:40 +0800

    测试stream

--- demo/src/main/java/com/example/demo/Controller/testController.java
+++ demo/src/main/java/com/example/demo/Controller/testController.java
@@ -1,8 +1,15 @@
 package com.example.demo.Controller;
 
+import org.springframework.util.StopWatch;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("test")
@@ -13,4 +20,71 @@ public class testController {
         return "hello!";
     }
 
+
+    public static void main(String[] args) {
+
+        Entity[] entities = new Entity[10000];
+        for(int i=0;i<entities.length;i++){
+            String age = "男";
+            if (i%2==1){
+                age = "女";
+            }
+            entities[i] = new Entity(Long.valueOf(i),"entity"+i,age);
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        List<Entity> list = Arrays.asList(entities);
+        StopWatch sw = new StopWatch();
+        sw.start("开始");
+        List<Entity> collect = list.stream().filter(e -> e.getId() % 2 == 0).collect(Collectors.toList());
+        sw.stop();
+        System.out.println(sw.prettyPrint());
+        System.out.println(sw.getTotalTimeMillis()+"");
+        //System.out.println(sw.getTaskInfo());
+        System.out.println(collect);
+
+    }
+
+
+}
+
+class Entity{
+    private Long id;
+    private String name;
+    private String age;
+
+    public Entity(Long id, String name, String age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
